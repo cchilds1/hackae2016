ebb4f83f53bd9b60153249a5171ba599d028390b
Layne Gustafson <laynegustafson97@gmail.com>
2016-11-05 17:10:58 -0400

    Add ProjectData.

---
+++ src/org/projectpost/data/ProjectData.java
@@ -0,0 +1,15 @@
+package org.projectpost.data;
+
+public class ProjectData {
+
+    public String uid;
+
+    public String name;
+    public String owner;
+    public int location;
+    public String time;
+    public String description;
+    public String image;
+    public int maxFunds;
+    public int currentFunds;
+}
