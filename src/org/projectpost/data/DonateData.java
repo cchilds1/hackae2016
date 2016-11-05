069dca47b1bcefe1ae27a877f3079674a48e5388
Jared Feeley <jfeeley1@binghamton.edu>
2016-11-05 17:19:53 -0400

    DonateData and PostcardData added

---
+++ src/org/projectpost/data/DonateData.java
@@ -0,0 +1,12 @@
+package org.projectpost.data;
+
+/**
+ * Created by Jared on 11/5/2016.
+ */
+public class DonateData {
+    public String uid;
+
+    public String user;
+    public String project;
+    public int donateAmount;
+}
---
+++ src/org/projectpost/data/PostcardData.java
@@ -0,0 +1,11 @@
+package org.projectpost.data;
+
+/**
+ * Created by Jared on 11/5/2016.
+ */
+public class PostcardData {
+    public String uid;
+
+    public String donation_id;
+    public String message;
+}
