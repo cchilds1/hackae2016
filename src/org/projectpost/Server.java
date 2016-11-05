268bee3f95c26d2a4156a08fc378dd1582c90175
Layne Gustafson <laynegustafson97@gmail.com>
2016-11-05 14:32:17 -0400

    Add hello world message for server

--- src/org/projectpost/Server.java
+++ src/org/projectpost/Server.java
@@ -1,4 +1,35 @@
 package org.projectpost;
 
-public class Server {
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+
+public class Server extends NanoHTTPD {
+
+    private final static int PORT = 8080;
+
+    public static void main(String[] args) {
+        try {
+            Server s = new Server();
+            s.start();
+        } catch (IOException ioe) {
+            System.err.println("Couldn't start server:\n" + ioe);
+        }
+    }
+
+
+    public Server() {
+        super(PORT);
+    }
+
+    public void start() throws IOException {
+        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        return newFixedLengthResponse("Hello World!");
+    }
+
+
 }
