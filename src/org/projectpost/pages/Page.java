d27e5c8c717da2b8c346798a7efec88d7bc721bc
Layne Gustafson <laynegustafson97@gmail.com>
2016-11-05 16:02:19 -0400

    Add user sessons, page default

--- src/org/projectpost/pages/Page.java
+++ src/org/projectpost/pages/Page.java
@@ -1,10 +1,19 @@
 package org.projectpost.pages;
 
 
+import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.router.RouterNanoHTTPD;
 import fi.iki.elonen.router.RouterNanoHTTPD.*;
 import fi.iki.elonen.NanoHTTPD.*;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.projectpost.Server;
+import org.projectpost.sessions.UserSession;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.Map;
 
 public abstract class Page extends DefaultHandler {
 
@@ -30,5 +39,21 @@ public abstract class Page extends DefaultHandler {
     @Override
     public Response.IStatus getStatus() {
         return Response.Status.OK;
+    }
+
+    @Override
+    public NanoHTTPD.Response get(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, NanoHTTPD.IHTTPSession session) {
+        UserSession us = new UserSession(null);
+        return NanoHTTPD.newFixedLengthResponse(getPage(uriResource, urlParams, us));
+    }
+
+    public abstract String getPage(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, UserSession session);
+
+    public String renderTemplate(String name, Map<String, Object> info) throws IOException, TemplateException {
+        Template t = Server.config.getTemplate(name);
+        StringWriter sw = new StringWriter();
+        t.process(info, sw);
+
+        return sw.toString();
     }
 }
---
+++ src/org/projectpost/sessions/UserSession.java
@@ -0,0 +1,18 @@
+package org.projectpost.sessions;
+
+public class UserSession {
+
+    private String uid;
+
+    public UserSession(String uid) {
+        this.uid = uid;
+    }
+
+    public boolean isLoggedIn() {
+        return uid != null;
+    }
+
+    public String getUID() {
+        return uid;
+    }
+}
