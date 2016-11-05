c01105f8d3e38d94546b32a03b46251d317c7011
Claire Childs <cchilds1@binghamton.edu>
2016-11-05 17:16:10 -0400

    add volunteer data

---
+++ src/org/projectpost/data/VolunteerData.java
@@ -0,0 +1,10 @@
+package org.projectpost.data;
+
+/**
+ * Created by Claire on 11/5/2016.
+ */
+public class VolunteerData {
+
+    public String uid;
+    public String user;
+    public String project;}
