1357e0f05197065404e036ec62be935436488e27
Yedidya Naiman <ddnaiman1@Dds-MacBook-Air.local>
2016-11-05 17:17:18 -0400

    add user data

---
+++ src/org/projectpost/data/UserData.java
@@ -0,0 +1,15 @@
+package org.projectpost.data;
+
+/**
+ * Created by ddnaiman1 on 11/5/16.
+ */
+public class UserData {
+    public String uid;
+    public String name;
+    public String username;
+    public String password;
+    public String email;
+    public String phoneNumber;
+    public String address;
+
+}
