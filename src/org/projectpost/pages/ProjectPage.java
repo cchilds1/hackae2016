15b00e4c5aea1ce0e92cbc5c63efdb0496c9d98c
Jared Feeley <jaredpcfeeley@gmail.com>
2016-11-06 02:40:39 -0500

    add ProjectPage class

--- src/org/projectpost/pages/ProjectPage.java
+++ src/org/projectpost/pages/ProjectPage.java
@@ -2,14 +2,36 @@ package org.projectpost.pages;
 
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.router.RouterNanoHTTPD;
+import org.projectpost.data.Database;
+import org.projectpost.data.ProjectData;
 import org.projectpost.sessions.UserSession;
 
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ProjectPage extends Page {
     @Override
     public NanoHTTPD.Response getPage(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, UserSession session) {
-        return null;
+        String projectID = urlParams.get("id");
+        try {
+
+            ProjectData pd = Database.getProjectData(projectID);
+            if(pd == null)
+            {
+                return newRedirectResponse("/");
+            }
+
+            Map<String, Object> projectMap = new HashMap<>();
+            projectMap.put("title", pd.name);
+            projectMap.put("owner", pd.owner);
+            projectMap.put("imageData", pd.image);
+            projectMap.put("description", pd.description);
+            String projectTemplate = renderTemplate("project.html", projectMap);
+            return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "text/html", projectTemplate);
+        } catch(Exception e){
+            return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.INTERNAL_ERROR, "text/plain", "failed to read template");
+        }
     }
 
     @Override
