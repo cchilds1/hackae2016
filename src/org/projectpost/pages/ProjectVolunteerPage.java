c47aa2b6ed2b5bc97bae3f9a889c2f1e91e5a33a
Claire Childs <cchilds1@binghamton.edu>
2016-11-06 02:36:07 -0500

    projectvolunteerpage

--- src/org/projectpost/pages/ProjectVolunteerPage.java
+++ src/org/projectpost/pages/ProjectVolunteerPage.java
@@ -2,18 +2,39 @@ package org.projectpost.pages;
 
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.router.RouterNanoHTTPD;
+import org.projectpost.data.Database;
+import org.projectpost.data.ProjectData;
 import org.projectpost.sessions.UserSession;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ProjectVolunteerPage extends Page {
     @Override
     public NanoHTTPD.Response getPage(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, UserSession session) {
-        return null;
-    }
+        if (!session.isLoggedIn())
+            return newRedirectResponse("/");
+        try {
+            String projectId = urlParams.get("id");
+            ProjectData pd = Database.getProjectData(projectId);
+            if(pd==null)
+                return newRedirectResponse("/");
+            Map<String, Object> volunteerMap = new HashMap();
+            volunteerMap.put("username", pd.owner);
+            volunteerMap.put("projectName", pd.name);
+            volunteerMap.put("location",pd.location);
+            volunteerMap.put("time", pd.time);
+            String volunteerTemplate = renderTemplate("volunteer.html", volunteerMap);
+            return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "text/html", volunteerTemplate);
+        }
+        catch(Exception e) {
+            return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.INTERNAL_ERROR, "text/plain", "failed to read template");
+        }
+        }
 
-    @Override
-    public NanoHTTPD.Response postPage(RouterNanoHTTPD.UriResource uriResource, Map<String, String> formParams, UserSession session) {
-        return null;
+        @Override
+        public NanoHTTPD.Response postPage (RouterNanoHTTPD.UriResource
+        uriResource, Map < String, String > formParams, UserSession session){
+            return null;
+        }
     }
-}
